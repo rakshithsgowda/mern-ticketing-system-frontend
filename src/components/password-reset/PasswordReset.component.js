bc21cc030a7e12245c77204923a60a734a28fd52
rakshith <rakshit.s.gowda@gmail.com>
2021-07-27 13:37:37 +0530

    reset password ui component

--- src/components/password-reset/PasswordReset.component.js
+++ src/components/password-reset/PasswordReset.component.js
@@ -1,53 +1,42 @@
 import { Col, Container, Form, Row, Button } from 'react-bootstrap'
 import { PropTypes } from 'prop-types'
 
-const LoginForm = ({
+const PasswordReset = ({
   handleOnChange,
-  handleOnSubmit,
   formSwitcher,
+  handleOnResetSubmit,
   email,
-  pass,
 }) => {
   return (
     <Container>
       <Row>
         <Col>
-          <h1 className='text-danger text-center'>Client Login</h1>
+          <h1 className='text-danger text-center'>Reset Password</h1>
           <hr />
-          <Form autoComplete='off' onSubmit={handleOnSubmit}>
+          <Form onSubmit={handleOnResetSubmit}>
             <Form.Group>
               <Form.Label>Email Address</Form.Label>
               <Form.Control
                 type='email'
                 name='email'
-                onChange={handleOnChange}
                 value={email}
-                placeholder='Enter Email'
-                // required
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type='password'
-                name='password'
                 onChange={handleOnChange}
-                value={pass}
-                placeholder='Enter password'
-                // required
+                placeholder='Enter Email'
               />
             </Form.Group>
+
             <Button className='mt-3' type='submit'>
-              Login
+              Reset Password
             </Button>
           </Form>
           <hr />
         </Col>
       </Row>
+
       <Row>
         <Col>
-          <a href='#!' onClick={() => formSwitcher('reset')}>
-            Forgot Password ?
+          <a href='#!' onClick={() => formSwitcher('login')}>
+            try Login now
           </a>
         </Col>
       </Row>
@@ -55,12 +44,11 @@ const LoginForm = ({
   )
 }
 
-LoginForm.propTypes = {
-  handleOnSubmit: PropTypes.func.isRequired,
+PasswordReset.propTypes = {
   handleOnChange: PropTypes.func.isRequired,
+  handleOnResetSubmit: PropTypes.func.isRequired,
   formSwitcher: PropTypes.func.isRequired,
   email: PropTypes.string.isRequired,
-  pass: PropTypes.string.isRequired,
 }
 
-export default LoginForm
+export default PasswordReset
--- src/pages/entry/Entry.page.js
+++ src/pages/entry/Entry.page.js
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import LoginForm from '../../components/login/Login.component'
+import PasswordReset from '../../components/password-reset/PasswordReset.component'
 import './entry.page.style.css'
 
 const Entry = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [formLoad, setFormLoad] = useState('login')
 
   const handleOnChange = (e) => {
     const { name, value } = e.target
@@ -27,20 +29,45 @@ const Entry = () => {
     if (!email || !password) {
       return alert('Fill up all the form elements!')
     }
-
     // TODO call api to submit the form
     console.log(email, password)
+  }
+
+  const handleOnResetSubmit = (e) => {
+    e.preventDefault()
+
+    if (!email) {
+      return alert('Please enter the email!')
+    }
+    // TODO call to api to submit
+    console.log(email)
+  }
+
+  const formSwitcher = (formType) => {
+    setFormLoad(formType)
   }
 
   return (
     <div className='entry-page bg-info'>
       <section className='jumbotron form-box p-5 bg-light'>
-        <LoginForm
-          handleOnSubmit={handleOnSubmit}
-          handleOnChange={handleOnChange}
-          email={email}
-          pass={password}
-        />
+        {formLoad === 'login' && (
+          <LoginForm
+            handleOnSubmit={handleOnSubmit}
+            handleOnChange={handleOnChange}
+            formSwitcher={formSwitcher}
+            email={email}
+            pass={password}
+          />
+        )}
+
+        {formLoad === 'reset' && (
+          <PasswordReset
+            handleOnChange={handleOnChange}
+            handleOnResetSubmit={handleOnResetSubmit}
+            formSwitcher={formSwitcher}
+            email={email}
+          />
+        )}
       </section>
     </div>
   )
