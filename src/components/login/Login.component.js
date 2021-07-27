5a72ffa6acb60af91b6e827a2034612009a05756
rakshith <rakshit.s.gowda@gmail.com>
2021-07-27 11:42:01 +0530

    login form component

--- src/components/login/Login.component.js
+++ src/components/login/Login.component.js
@@ -1,42 +1,55 @@
-import { Col, Container, Form, Row ,Button} from 'react-bootstrap'
-
-const Login = () => {
+import { Col, Container, Form, Row, Button } from 'react-bootstrap'
+import { PropTypes } from 'prop-types'
+const LoginForm = ({ handleOnChange, email, pass }) => {
   return (
     <Container>
       <Row>
         <Col>
-          <h1 className='text-danger text-center' >Client Login</h1>
+          <h1 className='text-danger text-center'>Client Login</h1>
           <hr />
           <Form>
             <Form.Group>
-              <Form.Label>
-                Email Address
-              </Form.Label>
-              <Form.Control type='email' name='email' placeholder='Enter Email' required />
-
-             
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control
+                type='email'
+                name='email'
+                onChange={handleOnChange}
+                value={email}
+                placeholder='Enter Email'
+                required
+              />
             </Form.Group>
             <Form.Group>
-              <Form.Label>
-               Password
-              </Form.Label>
-              <Form.Control type='password' name='password' placeholder='Enter password' required />
-
-             
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type='password'
+                name='password'
+                onChange={handleOnChange}
+                value={pass}
+                placeholder='Enter password'
+                required
+              />
             </Form.Group>
-            <Button className='mt-3' type='submit'>Login</Button>
+            <Button className='mt-3' type='submit'>
+              Login
+            </Button>
           </Form>
           <hr />
         </Col>
       </Row>
       <Row>
         <Col>
-          <a href="#!">Forgot Password ?</a>
+          <a href='#!'>Forgot Password ?</a>
         </Col>
-
       </Row>
     </Container>
   )
 }
 
-export default Login
+LoginForm.propTypes = {
+  handleOnChange: PropTypes.func.isRequired,
+  email: PropTypes.string.isRequired,
+  pass: PropTypes.string.isRequired,
+}
+
+export default LoginForm
--- src/pages/entry/Entry.page.js
+++ src/pages/entry/Entry.page.js
@@ -1,12 +1,38 @@
-import Login from '../../components/login/Login.component'
+import { useState } from 'react'
+import LoginForm from '../../components/login/Login.component'
 import './entry.page.style.css'
 
 const Entry = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target
+
+    switch (name) {
+      case 'email':
+        setEmail(value)
+        break
+      case 'password':
+        setPassword(value)
+        break
+
+      default:
+        break
+    }
+
+    console.log(name, value)
+  }
+
   return (
-    <div className='entry-page bg-info' >
-      <div className="jumbotron form-box p-5 bg-light">
-      <Login/>
-      </div>
+    <div className='entry-page bg-info'>
+      <section className='jumbotron form-box p-5 bg-light'>
+        <LoginForm
+          handleOnChange={handleOnChange}
+          email={email}
+          pass={password}
+        />
+      </section>
     </div>
   )
 }
